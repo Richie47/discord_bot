0caf62b6245ffdf577f65d68d736f2a6c33a8bd7
Richie <richiemck47@gmail.com>
2019-02-22 18:23:32 -0700

    update reddit scraper

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,45 +1,70 @@
-"""
-Several miscellanious commands below, most will be free to use by all users as they are harmless
-fun commands
-"""
-
-import discord
+import discord 
+import requests
+import sys
+import traceback
+import time
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
 
-class FunCog:
-    def __init__(self,bot):
-        self.bot = bot
+extensions = ['cogs.members', 'cogs.fun', 'cogs.guilds', 'cogs.music']
 
-    @bot.command(alaises=['bc', 'bit'])
-    async def bitcoin(ctx):
-        """See the live time value of BitCoin!"""
-        url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-        response = requests.get(url)
-        value = response.json()['bpi']['USD']['rate']
-        await ctx.send(f'Bitcoin price is currently ${value}')
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+            print(f'{extension} loaded!')
+        except Exception as e:
+            print(f'Warning: failed to load extension {extension}.', file = sys.stderr)
+            traceback.print_exc()
 
-    @commands.command(name='embeds')
-    @commands.guild_only()
-    async def example_embed(self, ctx):
-        """A simple command which showcases the use of embeds.
-        Have a play around and visit the Visualizer."""
-
-        embed = discord.Embed(title='Example Embed',
-                              description='Showcasing the use of Embeds...\nSee the visualizer for more info.',
-                              colour=0x98FB98)
-        embed.set_author(name='MysterialPy',
-                         url='https://gist.github.com/MysterialPy/public',
-                         icon_url='http://i.imgur.com/ko5A30P.png')
-        embed.set_image(url='https://cdn.discordapp.com/attachments/84319995256905728/252292324967710721/embed.png')
-
-        embed.add_field(name='Embed Visualizer', value='[Click Here!](https://leovoel.github.io/embed-visualizer/)')
-        embed.add_field(name='Command Invoker', value=ctx.author.mention)
-        embed.set_footer(text='Made in Python with discord.py@rewrite', icon_url='http://i.imgur.com/5BFecvA.png')
-
-        await ctx.send(content='**A simple Embed for discord.py@rewrite in cogs.**', embed=embed)
+@bot.event
+async def on_ready():
+    print(f'Logged in as: {bot.user.name}')
+    print(f'ID: {bot.user.id}')
+    print(f'Version {discord.__version__}')
+    await bot.change_presence(activity = discord.Streaming(name='Making a music bot!', url='https://www.twitch.tv/twitchpresents', type = 1))
 
 
-def setup(bot):
-    bot.add_cog(FunCog(bot))
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    """The event triggered when an error is raised while invoking a command.
+    ctx   : Context
+    error : Exception"""
+
+    print(error)
+    if hasattr(error, 'NameError'):
+        await ctx.send('Name is not defined, try again.')
+        print('nameError occured')
+
+    await ctx.send('Did you really think I would let you do that? :thinking:')
+
+
+
+@bot.command()
+async def ping(ctx):
+    """See if there's a reason why I'm running slow"""
+    await ctx.send(f'Pong! {round(bot.latency, 2)}s response time.')
+
+
+#TODO: Make a clear messages cmd, create channels/channel management, mute/ban/deaf members
+
+@bot.command()
+async def roles(ctx):
+    await ctx.send(f' {ctx.guild.roles} ')
+
+
+@bot.command()
+async def dm(ctx, member : discord.Member, msg : str):
+    if member is None:
+        await ctx.send("You didn't specify a member!")
+
+    elif msg is None:
+        await ctx.send("You didn't supply a message to send!")
+    
+    else:
+        await member.send(msg)   
+
+bot.run('NDgzMTI1NDcyNzgwMjIyNDk1.DypJFg.XJKogfY77Yr4jP4xj8tn5Kwr6nU', bot=True, reconnect=True)
